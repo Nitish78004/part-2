80b06d0e0801f958d967d1ccc509b11cb75bbed4
Nitesh Singh <your_github_email@example.com>
2025-11-12 18:53:48 -0800

    first commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 export const dbConnect = async () => {
-    const con = await mongoose.connect('mongodb://localhost:27017/Quirex')
+    const con = await mongoose.connect('mongodb+srv://nknitishsingh92_db_user:Nitish780045@clusterone.7zg3loj.mongodb.net/')
     if (con) {
         console.log("Db connected sucessfully.........")
     }
